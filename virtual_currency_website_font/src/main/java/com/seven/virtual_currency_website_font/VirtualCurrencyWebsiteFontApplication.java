ca6c4c1a07023eb842b8c521b0a7dfd1f77f7fc5
lumiseven <lumiseven@yahoo.com>
2017-08-31 14:32:42 +0800

    some test code deleted.

--- virtual_currency_website_font/src/main/java/com/seven/virtual_currency_website_font/VirtualCurrencyWebsiteFontApplication.java
+++ virtual_currency_website_font/src/main/java/com/seven/virtual_currency_website_font/VirtualCurrencyWebsiteFontApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-import com.seven.virtual_currency_website_data.VirtualCurrencyWebsiteDataApplication;
+import com.seven.virtual_currency_website.data.App;
 
 @SpringBootApplication
-@Import(VirtualCurrencyWebsiteDataApplication.class)
+@Import(App.class)
 public class VirtualCurrencyWebsiteFontApplication {
 
 	public static void main(String[] args) {
